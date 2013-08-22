9ed45386062b3bfcf801feb5d37cab993c4aa89e
Nick Dedekind <nicholas.dedekind@gmail.com>
2013-08-22 20:36:50 +0100

    Added copywrite headers

--- libqmenumodel/src/gtk/gtkactionobserveritem.h
+++ libqmenumodel/src/gtk/gtkactionobserveritem.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2011 Canonical Limited
+ * Copyright © 2013 Canonical Limited
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
