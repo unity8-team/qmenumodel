4f4fde02f90abeb1d20a842b2a7578a84fe75f51
Nick Dedekind <nicholas.dedekind@gmail.com>
2014-07-01 11:17:23 +0100

    fixed copywrite

--- tests/client/unitymenuactiontest.cpp
+++ tests/client/unitymenuactiontest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Canonical Ltd.
+ * Copyright 2014 Canonical Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authors:
- *      Renato Araujo Oliveira Filho <renato@canonical.com>
+ *      Nick Dedekind <nick.dedekind@canonical.com>
  */
 
 #include "unitymenumodel.h"
